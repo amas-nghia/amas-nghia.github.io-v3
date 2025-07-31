840bed986e7858871d18520cf7a1b54b7dc6afbd
Nguyễn Thành Nghĩa <amasnghia@gmail.com>
2025-07-31 23:26:10 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,15 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  assetPrefix: isProd ? '/amas-nghia.github.io/' : '',
+  basePath: isProd ? '/amas-nghia.github.io' : '',
 }
 
 export default nextConfig
